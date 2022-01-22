e1f795419c7cf872afd73d5f72bb6f07562a2786
unknown <janeaustine0@gmail.com>
2022-01-22 10:31:47 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return // return liksi but variable nabe dei nai tokonoo undefined dekhabeee
+}
+
+const result = add(13, 82); // function theke jonkon kono kisu return na kora hoy tahole seeta undefined dekhabeeee
+const result1 = add(13); // jodi kono function er parameter 1 ta deya hoy ar 1 deya na hoy tahole na deyr value ta undefined dekhabeee
+console.log(result);
+
+const premik = {name: 'smart dude', phone: 3434343};
+console.log(premik.gf); // object er property jodi na deya hoy taholee undefined dekhabeee
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 6];
+console.log(ages[11]);
