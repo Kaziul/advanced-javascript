02da35848ab7a7bb65192b889d1123e09daf4deb
unknown <janeaustine0@gmail.com>
2022-01-22 10:09:43 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// ########### Falsy #####
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// ########### Truthy #####
+// '0', ' ', [],{}
+const age = 4;
+if (age > 0) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
